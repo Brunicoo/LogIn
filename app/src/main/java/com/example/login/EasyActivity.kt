fb61a1bb439e34e9877eb1373ea73ad1dc3c41c8
Brunicoo <brunoconvaliarejas7@gmail.com>
2024-11-18 19:57:38 +0100

    login

---
+++ app/src/main/java/com/example/login/EasyActivity.kt
@@ -0,0 +1,13 @@
+package com.example.login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class EasyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_easy)
+    }
+}
--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -1,20 +1,61 @@
 package com.example.login
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btonEasy = findViewById(R.id.btonEasy) as Button
+        val btonHard = findViewById(R.id.btonHard) as Button
+        val btonStart = findViewById(R.id.btonStart) as Button
+        val userNameEditText = findViewById(R.id.userName) as EditText
+        var difficulty : Boolean? = null
+
+        btonEasy.setOnClickListener()
+        {
+            difficulty = false
+            btonEasy.setBackgroundResource(R.drawable.button_selected_easy)
+            btonHard.setBackgroundResource(R.drawable.rectangle_borders2)
+        }
+
+        btonHard.setOnClickListener()
+        {
+            difficulty = true
+            btonHard.setBackgroundResource(R.drawable.button_selected_hard)
+            btonEasy.setBackgroundResource(R.drawable.rectangle_borders)
+        }
+
+        btonStart.setOnClickListener()
+        {
+            if (userNameEditText.text.isEmpty())
+            {
+                Toast.makeText(this, "¡You must enter your name!", Toast.LENGTH_LONG).show()
+            }
+            else {
+                if (difficulty != null) {
+                    if (difficulty == false) {
+                        val intent = Intent(this, EasyActivity :: class.java)
+                        startActivity(intent)
+
+                    } else if (difficulty == true) {
+                        //abrir activity del hard
+                    }
+                } else {
+                    Toast.makeText(this, "¡You must select a difficulty!", Toast.LENGTH_LONG).show()
+                }
+            }
         }
     }
 }
